54c4bea5dca57331ea2d8b559fd3c365a2e862d6
MaikeraW <wagner.maiconhenrique@gmail.com>
2026-05-15 14:04:37 -0300

    iniciado projeto de site para os Bombeiros Voluntários de Ilhota

---
+++ scripts/scripts.js
@@ -0,0 +1,30 @@
+let currentIndex = 0;
+const slides = document.querySelector('.slides');
+const dots = document.querySelectorAll('.dot');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+function updateSlider() {
+  slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+
+  dots.forEach(dot => dot.classList.remove('active'));
+  dots[currentIndex].classList.add('active');
+}
+
+function goToSlide(index) {
+  currentIndex = index;
+  updateSlider();
+  resetAutoSlide();
+}
+
+// Auto slide
+let interval = setInterval(nextSlide, 5000);
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % totalSlides;
+  updateSlider();
+}
+
+function resetAutoSlide() {
+  clearInterval(interval);
+  interval = setInterval(nextSlide, 4000);
+}
